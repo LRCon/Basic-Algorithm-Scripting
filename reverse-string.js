21533db64c3799156e5152d5aaee259d260319c4
unknown <luisramcontreras@gmail.com>
2018-05-23 12:28:20 -0700

    reduced amount of code by using methods instead

--- reverse-string.js
+++ reverse-string.js
@@ -1,13 +1,5 @@
 function reverseString(str) {
-  var string = str;
-  var newString = "";
-  var strArr = [];
-  for(i=0; i<string.length; i++){
-    strArr.push(string[i]);
-  }
-  for(x=strArr.length-1; x>=0; x--){
-      newString += strArr[x];
-   }
+  var newString = str.split("").reverse().join("");
   return newString;
 }
 
